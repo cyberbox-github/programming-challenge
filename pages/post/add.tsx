a1156e8af9d55f2cceb1e68f3de3d504d0dca2e1
CYBERBOX <139312052+cyberbox-github@users.noreply.github.com>
2023-08-28 08:57:04 +0100

    Revert "Feat: Task completed"

--- pages/post/add.tsx
+++
@@ -1,5 +0,0 @@
-import { AddEditPostForm } from '@components/post/addEditPostForm'
-
-export default function Add() {
-  return <AddEditPostForm />
-}
--- pages/post/edit.tsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-import { AddEditPostForm } from '@components/post/addEditPostForm'
-import { usePostContext } from '@context/PostProvider'
-
-export default function EditPost() {
-  const postContext = usePostContext()
-
-  return <AddEditPostForm post={postContext.post} />
-}
